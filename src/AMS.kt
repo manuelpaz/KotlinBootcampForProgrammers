cc877f53abe9ff5570452fe3ba3067f9fbce0053
Manuel Paz Robles <manuelpazrobles@gmail.com>
2018-11-02 16:05:58 -0600

    Se modifica el archivo AMS.kt la función main recibira un argumento de la hora en formato 24 horas y dependiendo del valor se dara el saludo de buenos días o el saludo de buenas noches el objetivo de esta práctica es el  de utilizar la caracterista de que casi todo en Kotlin devuelve un valor.

--- src/AMS.kt
+++ src/AMS.kt
@@ -1,13 +1,10 @@
+//Cree una función main () que tome un argumento que represente la hora en formato de 24 horas (valores entre e incluyendo 0 -> 23).
+//En la función main (), compruebe si la hora es anterior al mediodía (<12), luego imprima "Good morning, Kotlin"; de lo contrario,
+// imprima “Good night, Kotlin".
+//Notas:
+//Recuerde que todos los argumentos de la función main () son cadenas, por lo que tendrá que convertir este argumento en un Int antes de poder aplicar la verificación.
+//Avanzado
+//Trate de usar las plantillas de cadenas de Kotlin para hacer esto en 1 línea.
 fun main(args: Array<String>){
-    println("Hello, ${args[0]}")
-
-    val isUnit = println("This is an expression")
-    println(isUnit)
-
-    val temperature = 10
-    val isHot = if(temperature>50) true else false
-    println(isHot)
-
-    val message = "You are ${ if (temperature > 50) "fried" else "safe"} fish"
-    println(message)
+    println(if(args[0].toInt()<12) "Good moring Kotlin" else "Good Night Kotlin")
 }
