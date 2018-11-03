6d4ad0ce7bb257c798939a6cbd99e59a53aa4a92
Manuel Paz Robles <manuelpazrobles@gmail.com>
2018-11-02 18:03:38 -0600

    Se extiende la práctica de la galleta de la fortuna utilizando un ciclo for para que se repita 10 veces la ejecución del metodo getFortuneCookie o hasta que se selecciones la cadena "Take it easy ..."

--- src/Cookie.kt
+++ src/Cookie.kt
@@ -1,27 +1,11 @@
-//Cree un programa con una función que devuelva un mensaje de cookie de fortuna que pueda imprimir.
-//Crear una función main ().
-//Desde la función main (), llame a una función, getFortuneCookie (), que devuelve una cadena.
-//Cree una función getFortuneCookie () que no tome argumentos y devuelva un String.
-//En el cuerpo de getFortuneCookie (), crea una lista de fortunas. Aquí hay algunas ideas:
-//"You will have a great day!"
-//"Things will go well for you today."
-//"Enjoy a wonderful day of success."
-//"Be humble and all will turn out well."
-//"Today is a good day for exercising restraint."
-//"Take it easy and enjoy life!"
-//"Treasure your friends because they are your greatest fortune."
-//Sugerencia: use print (), no println ()
-//Crear una variable, birthday.
-//Lea la entrada del usuario de la entrada estándar y asígnele a birthday Si no hay una entrada válida, establezca birthday en 1.
-//Sugerencia: use readLine () para leer una línea de entrada (completada con Enter) como una cadena.
-//Sugerencia: en Kotlin, puede usar toIntOrNull () para convertir un número como una cadena a un número
-// entero. Si el usuario ingresa "", toIntOrNull devuelve nulo.
-//Sugerencia: compruebe si hay null utilizando el ? Operador y use el operador?: para manejar el caso nulo.
-//Divida birthay por el número de fortunes y use el resto como el índice de la fortuna para regresar.
-//retorna fortune.
-//En main (), escriba: "Your fortune is:", seguida de la cadena de fortune.
+//Praáctica extra: Use un bucle for para ejecutar el programa 10 veces, o hasta que la fortuna "Take it easy" haya sido seleccionada.
 fun main(args: Array<String>){
-        println("\nYour fortune is: ${getFortuneCookie()}")
+    var fortune: String
+    for (i in 1..10) {
+        fortune = getFortuneCookie()
+        println("\nYour fortune is: $fortune")
+        if (fortune.contains("Take it easy and enjoy life!") ) break
+    }
 }
 fun getFortuneCookie(): String{
     val fortunes = listOf("You will have a great day!",
