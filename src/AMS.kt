e2d5c710d3b94c6bcc235e9fc046827098848d32
Manuel Paz Robles <manuelpazrobles@gmail.com>
2018-11-03 21:50:36 -0600

    Se agrega la función randomDay extendiendo la funcionalidad en el archivo AMSkt que alimenta los peces pero de manera aleatoria,

--- src/AMS.kt
+++ src/AMS.kt
@@ -1,10 +1,16 @@
+import java.util.*
+
 fun main(args: Array<String>){
     println("Hello, ")
     feedTheFish()
 }
 fun feedTheFish(){
-    val day = "Tuesday"
+    val day = randomDay()
     val food = "pellets"
     println("Today is $day and the fish eat $food")
 }
 
+fun randomDay(): String{
+    val week = listOf("Moday", "Tuesday", "Wednesday", "Thursday", "Saturday", "Sunday")
+    return week[Random().nextInt(7)]
+}
