4bb191c84be4737f11c8067009338c97b8dc010c
Manuel Paz Robles <manuelpazrobles@gmail.com>
2018-11-04 19:30:46 -0600

    Se modifica el archvio Cookie agregando una función getBirthday el cual se encarga de obetener el dia de cumpleaños y lo devuelve como parametro a getFortune. Ademas la fortuna se devuelve a través de una declaración when

--- src/Cookie.kt
+++ src/Cookie.kt
@@ -2,20 +2,31 @@
 fun main(args: Array<String>){
     var fortune: String
     for (i in 1..10) {
-        fortune = getFortuneCookie()
+        fortune = getFortuneCookie(getBirthDay())
         println("\nYour fortune is: $fortune")
         if (fortune.contains("Take it easy and enjoy life!") ) break
     }
 }
-fun getFortuneCookie(): String{
-    val fortunes = listOf("You will have a great day!",
+fun getFortuneCookie(birthDay: Int): String{
+     val fortunes = listOf("You will have a great day!",
             "Things will go well for you today.",
             "Enjoy a wonderful day of success.",
             "Be humble and all will turn out well.",
             "Today is a good day for exercising restraint",
             "Take it easy and enjoy life!",
             "Treasure your friends because they are your greatest fortune.")
+
+    val index  = when(birthDay){
+        in 1..7 -> 4
+        28,31 -> 2
+        in 25..27 -> 1
+        in 8..18 -> 6
+        else -> birthDay.rem(fortunes.size)
+    }
+  return fortunes[index]
+}
+
+fun getBirthDay(): Int{
     print("Enter your birthday:")
-    val birthday = readLine()?.toIntOrNull() ?: 1
-    return fortunes[birthday.rem(fortunes.size)]
+    return readLine()?.toIntOrNull() ?: 1
 }
