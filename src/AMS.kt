a4d28f903d1176cc4f36ac92d31f0a91f25e8b80
Manuel Paz Robles <manuelpazrobles@gmail.com>
2018-11-13 19:51:41 -0600

    Se agrega la función canAddFish en el archivo AMSktla cual permite verificar si es posible agregar un pez tomando en cuenta la regla de una-pulgada-por-pez-por-galón-de-agua.

--- src/AMS.kt
+++ src/AMS.kt
@@ -3,6 +3,7 @@ import java.util.*
 fun main(args: Array<String>){
     println("Hello, ")
     feedTheFish()
+    canAddFish(10, listOf(3,3,3))
 }
 fun shouldChangeWater(
         day: String,
@@ -57,5 +58,21 @@ fun fishFood(day: String):String{
        "Saturday" -> "lettuce"
        "Sunday" -> "plankton"
         else -> "fasting"
+    }
+}
+
+/*Función fitMorefish
+Crear una función que reciba estos argumentos:
+tankSize (en galones)
+currentfish (una lista de Ints que representen la longitud de cada pez que hay actualmente en el tanque)
+fishSize (La longitud de los peces nuevos que queremos agregar al tanque)
+hasDecorations (true si el tanque tiene decoraciones, false si no)
+Puedes asumir que tipicamente un tanque tiene decoraciones, y que un pez típico es de 2 pulgadas de largo. Esto significa puedes establecer estos valores como parámetros por defecto.
+*/
+
+fun canAddFish(tankSize: Int, currentFish: List<Int>, fishSize: Int = 2, hasDecorations: Boolean = true):Boolean{
+    return when(hasDecorations){
+        true -> tankSize*0.8 >= currentFish.sum() + fishSize
+        else -> tankSize >= currentFish.sum() + fishSize
     }
 }
